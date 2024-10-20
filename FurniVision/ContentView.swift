d66d6467760d01b50b5d5b9518dc537b7b82dc38
iAboud98 <aboudfialah98@gmail.com>
2024-10-21 01:43:45 +0300

    add first page buttons

--- FurniVision/ContentView.swift
+++ FurniVision/ContentView.swift
@@ -9,16 +9,78 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        ZStack {
+            Color.brown
+                .ignoresSafeArea()
+
             VStack {
-                Image(systemName: "globe")
-                    .imageScale(.large)
-                    .foregroundStyle(.tint)
-                Text("Hello, world!")
+                Text("Welcome to FurniVision")
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+                    .padding(.top, 120)
+                    
+
+                Spacer()
+                
+                Text("Select a category to start")
+                    .font(.title)
+                    .foregroundColor(.white)
+                
+                Grid {
+                    GridRow {
+                        createButton(title: "chair")
+                        createButton(title: "table")
+                    }
+                    GridRow {
+                        createButton(title: "closet")
+                        createButton(title: "bed")
+                    }
+                    GridRow {
+                        createButton(title: "couch")
+                        createButton(title: "vase")
+                    }
+                }
+                .padding(.bottom, 0)
+
+                HStack {
+                    Button(action: {
+                        // Define button action here
+                    }) {
+                        Text("next")
+                            .frame(width: 368, height: 80)
+                            .background(Color(red: 150/255, green: 110/255, blue: 65/255))
+                            .foregroundColor(.black)
+                            .cornerRadius(10)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(Color.black, lineWidth: 2)
+                            )
+                    }
+                    .padding(.horizontal)
+                }
+                .padding(.bottom, 20)
             }
-            .padding()
         }
+    }
+
+    func createButton(title: String) -> some View {
+        Button(action: {
+            // Define button action here
+        }) {
+            Text(title)
+                .frame(width: 180, height: 120)
+                .background(Color(red: 150/255, green: 110/255, blue: 65/255))
+                .foregroundColor(.black)
+                .cornerRadius(10)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(Color.black, lineWidth: 2)
+                )
+        }
+        .padding(0)
+    }
 }
 
 #Preview {
-    splashScreen()
+    ContentView()
 }
