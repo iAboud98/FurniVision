9349b5fdf330f5d9a822ef9ce3918656bf26bd98
iAboud98 <aboudfialah98@gmail.com>
2024-10-21 00:43:35 +0300

    create splash screen

---
+++ FurniVision/splashScreen.swift
@@ -0,0 +1,42 @@
+//
+//  splachScreen.swift
+//  FurniVision
+//
+//  Created by Aboud Fialah on 21/10/2024.
+//
+
+import SwiftUI
+
+struct splashScreen: View {
+    @State var isActive : Bool = false
+        
+        var body: some View {
+            ZStack {
+                
+                Color.brown
+                    .edgesIgnoringSafeArea(.all)
+                
+                if self.isActive {
+                    ContentView()
+                }else{
+                    Image("logo")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 300, height: 300)
+                        .offset(x: 0, y: -50)
+                    
+                }
+            }
+            .onAppear() {
+                DispatchQueue.main.asyncAfter(deadline: .now()+3){
+                    withAnimation(){
+                        self.isActive = true
+                    }
+                }
+            }
+        }
+}
+
+#Preview {
+    splashScreen()
+}
