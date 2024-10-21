5bb8062cae50ad072d12e85a620c4d1d894c4bbc
iAboud98 <aboudfialah98@gmail.com>
2024-10-21 18:08:30 +0300

    fix splash screen and FurniVisionApp file

--- FurniVision/FurniVisionApp.swift
+++ FurniVision/FurniVisionApp.swift
@@ -9,9 +9,17 @@ import SwiftUI
 
 @main
 struct FurniVisionApp: App {
+    @State private var isActive: Bool = false // State variable to manage the splash screen state
+
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ZStack {
+                if isActive {
+                    ContentView() // Show the main content view after the splash screen
+                } else {
+                    splashScreen(isActive: $isActive) // Show the splash screen
+                }
+            }
         }
     }
 }
