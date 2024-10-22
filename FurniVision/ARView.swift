0584daa1d1315bbda56d0129a1bf9e455907a38c
iAboud98 <aboudfialah98@gmail.com>
2024-10-23 00:39:32 +0300

    create ARVision file

---
+++ FurniVision/ARView.swift
@@ -0,0 +1,18 @@
+//
+//  ARView.swift
+//  FurniVision
+//
+//  Created by Aboud Fialah on 23/10/2024.
+//
+
+import SwiftUI
+
+struct ARView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ARView()
+}
